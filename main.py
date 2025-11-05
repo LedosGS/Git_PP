a999f5e3b0df43b9c9caaeb4bdfda1598a281634
Ledos <artempax2007@gmail.com>
2025-11-05 16:29:44 +0300

    wait for unit test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 
-
+import requests
 
 hex_re = re.compile(r'\b#?([A-Fa-f0-9]{3}|[A-Fa-f0-9]{4}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})\b')
 
@@ -22,9 +22,20 @@ def hex_in_file():
     except Exception as E:
         print("ошибка открытия файла", E)
 
+def hex_in_Web(url: str):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        page = response.text
+        hex_in_string(page)
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе: {e}")
+        return None
 
-hex_in_string("##1230,#112")
+
+hex_in_string("##1230,112")
 hex_in_file()
+hex_in_Web("https://colorscheme.ru/html-colors.html")
 
 
 
